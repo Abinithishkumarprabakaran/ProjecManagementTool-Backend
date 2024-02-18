e3330ee6127251fd3f0ac4df2cb46a4ce3fbcbe2
abinithishkumarprabakaran <abinithishkumarp.a.k@gmail.com>
2024-02-18 12:43:47 +0530

    publish

--- index.js
+++ index.js
@@ -28,9 +28,7 @@ const origin = process.env.CORS_ORIGIN || 'http://localhost:5173/'
 
 const app = express()
 
-app.use(cors({
-    origin
-}));
+app.use(cors());
 app.use(express.json())
 // app.use(express.urlencoded())
 
